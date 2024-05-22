ed3e7a723188251f0f094545bf94d31cc8ab0f27
Anthony Nguyen <anthonyng041@gmail.com>
2024-05-22 14:28:34 -0400

    Added Semantic UI

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Header, List } from "semantic-ui-react";
 
 export default function Home() {
   const [activities, setActivities] = useState([])
@@ -12,7 +13,7 @@ export default function Home() {
   }, [])
 
   return (
-    <div><ul>{activities.map((activity: any) => ( <li key={activity.id}>{activity.title}</li> ))}</ul></div>
+    <div><Header as='h2' icon='users' content='Reactivities'/><List>{activities.map((activity: any) => ( <List.Item key={activity.id}>{activity.title}</List.Item> ))}</List></div>
   );
 }
 
