16d75d478d16aba489928b6dd5218eb187735c95
Anthony Nguyen <anthonyng041@gmail.com>
2024-05-26 15:22:11 -0400

    End of Section 4

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Application.Activities;
+using Application.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection Services, IConfiguration config)
+        {
+            Services.AddEndpointsApiExplorer();
+            Services.AddSwaggerGen();
+            Services.AddDbContext<Persistence.DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            Services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+
+            Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(List.Handler).Assembly));
+
+            Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return Services;
+        }
+    }
+}
